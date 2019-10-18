56991decac4161993e3ca9bec8d4cd644f9a1501
Lisa Lee <llee28@cnm.edu>
2019-10-17 22:14:15 -0600

    added milestone 1

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>PWP Milestone 1</title>
+</head>
+<body>
+<h1 class="center"><strong>Personal Web Page - Milestone 1</strong></h1>
+<div>
+	<h2>Specific Purpose</h2>
+		<ul>
+			<li>Attract potential clients by showcasing my abilities as a web and content developer/designer</li>
+		</ul>
+</div>
+<div>
+	<h2>Audience</h2>
+		<ul>
+			<li>Potential customers, specifically individuals and small businesses in Albuquerque and the surrounding areas</li>
+		</ul>
+</div>
+<div>
+	<h2>Goal</h2>
+		<ul>
+			<li>Attract potential freelance clients by highlighting my abilities and services on my pwp</li>
+		</ul>
+</div>
+<div>
+	<h2>Persona</h2>
+		<ul>
+			<li>Julian is a small business owner, he's been a restauranteur for 5 years (vegetarian cuisine)</li>
+			<li>Has a healthy number of regular customers, but has room to grow</li>
+			<li>Wants to invest in his business by advertising and wants a web page for his business, but does not want to maintain it
+			<li>Not only busy with managing his restaurant, but has a wife and two kids (ages 5 and 10)</li>
+			<li>Besides, spending time with his family and friends, he enjoys fishing, hiking, cooking, and gardening</li>
+			<li>48 years of age</li>
+			<li>Uses a PC laptop, Apple iPhone, but does not consider himself tech savvy</li>
+			<li>Struggles with prioritizing things in his business versus his personal life</li>
+			<li>Likes to juggle to relax</li>
+			<li>Reads like crazy, especially news and sports online</li>
+		</ul>
+</div>
+</body>
+</html>
