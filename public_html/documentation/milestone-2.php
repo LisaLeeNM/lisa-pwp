20501facf3783f66032f92732e040e75455dfb8d
Lisa Lee <llee28@cnm.edu>
2019-10-29 21:31:36 -0600

    add sections for content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,29 +7,21 @@
 <body>
 <h1><strong>Personal Web Page - Milestone 2</strong></h1>
 <div>
-	<h2></h2>
-	<ul>
-		<li></li>
-	</ul>
-</div>
+	<h2>Content Strategy</h2>
+	<p>My personal site will be one page and will consist of the following sections:</p>
+		<h3>Featured Content</h3>
 
-<div>
-	<h2></h2>
-	<ul>
-		<li></li>
-	</ul>
-</div>
+			<h4>Welcome</h4>
+			<p>This section will have a brief introduction with my name and role as a Fullstack Developer. This will be overlaid a soft gray background.</p>
 
-<div>
-	<h2></h2>
-	<ul>
-		<li></li>
-	</ul>
-</div>
+			<h4>About Me</h4>
+			<p>This section will detail my professional history, core skill set, and professional highlights. This section will showcase my strengths and personality. I will feature a contrasting sidebar that lists my core skills and programming languages which will be easily readable at a glance. The tone of voice will be personable and casual, as I am only interested in opportunities with individuals and organizations that I am a good personality fit for.</p>
 
-<div>
-	<h2></h2>
-</div>
+			<h4>Portfolio</h4>
+			<p>I want this to be an animated, responsive image gallery containing 4-6 examples of my best work. Gallery pop-up will include my role and contributions to each project, with iconography to represent the technology used.</p>
+
+			<h4>Contact</h4>
+			<p>This section will contain a simple contact form, my professional email address, and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.)</p>
 
 </body>
 </html>
