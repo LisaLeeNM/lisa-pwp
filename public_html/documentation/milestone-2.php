7764bad0b2df3d6eb29239cc6700e427228d32e4
Lisa Lee <llee28@cnm.edu>
2019-10-29 20:59:54 -0600

    add template html

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>PWP Milestone 2</title>
+</head>
+<body>
+<h1><strong>Personal Web Page - Milestone 2</strong></h1>
+<div>
+	<h2></h2>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+
+<div>
+	<h2></h2>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+
+<div>
+	<h2></h2>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+
+<div>
+	<h2></h2>
+</div>
+
+</body>
+</html>
