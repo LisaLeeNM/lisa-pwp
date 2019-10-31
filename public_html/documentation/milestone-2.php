76943c03a221b8125a30e1fd6ee1f185f7ea8957
Lisa Lee <llee28@cnm.edu>
2019-10-31 06:53:48 -0600

    change font family

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -2,34 +2,32 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8"/>
+	<style>
+		* {
+			font-family: sans-serif;
+		}
+	</style>
 	<title>PWP Milestone 2</title>
 </head>
 <body>
 <h1><strong>Personal Web Page - Milestone 2</strong></h1>
 <div>
 	<h2>Content Strategy</h2>
-	<p>My personal site will be one page and will consist of the following sections:</p>
-		<h3>Featured Content</h3>
+	<p>My personal site will be one page where one can scroll down to the different sections; it will consist of the following:</p>
 
 			<h4>Welcome</h4>
-			<p>This section will have my name handwritten or stylized with my role as a Fullstack Developer at the top and text/links in orange on the bottom for 'about', 'portfolio', and 'contact'. This will be overlaid over a soft gray background with a logo or imagery.</p>
+			<p>This will have my name handwritten or stylized with my role as a Fullstack Developer over a large background image. At the top, will be my tagline and the hamburger dropdown that brings viewer down to the 'about', 'portfolio', and 'contact'. Social media icons shown at the bottom.</p>
 
 			<h4>About</h4>
-			<p>This section will detail my professional history, core skill set, and professional highlights. This section will showcase my strengths and personality. I will feature a contrasting sidebar that lists my core skills and programming languages which will be easily readable at a glance. The tone of voice will be personable and casual, as I am only interested in opportunities with individuals and organizations that I am a good personality fit for.</p>
+			<p>This will give viewers a brief overview about me (giving them a sense of my personality), and include my skill set and professional highlights. The tone will be personable and casual, as I am interested in opportunities with individuals and organizations that I am a good personality fit for. This will be blocked with a pop of color with the tagline and hamburger icon at the top and the social media icons at the bottom; these will appear throughout.</p>
 
 			<h4>Portfolio</h4>
-			<p>I want this to be an animated, responsive image gallery containing 4-6 examples of my best work. Gallery pop-up will include my role and contributions to each project, with iconography to represent the technology used.</p>
+			<p>I want this to be a responsive image gallery containing 3 examples of my best work. Gallery pop-up will include my role and contributions to each project, with iconography to represent the technology used.</p>
 
 			<h4>Contact</h4>
-			<p>This section will contain a simple contact form, my professional email address, and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.)</p>
-
-			<h4>Contact</h4>
-			<p>This section will contain a simple contact form, my professional email address, and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.)</p>
+			<p>This section will contain a simple contact form.</p>
 
 	<h2>Wireframes</h2>
-	<ul></ul>
-	<li>Mobile Layout: xxx</li>
-	<li>Web Layout: xxx</li>
-	<ul></ul>
+	<a href="https://balsamiq.cloud/spdns04/pzi3u32">Layouts</a>
 </body>
 </html>
