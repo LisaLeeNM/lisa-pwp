7851b8c39723d3f0ee3ac43048c064fe01000aa8
Lisa Lee <llee28@cnm.edu>
2019-11-10 19:27:07 -0700

    add draft text for jumbotron page

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -22,7 +22,7 @@
 		<header>
 			<div class="container pt-3">
 				<nav class="navbar navbar-expand-lg navbar-light bg-light">
-					<a class="navbar-brand" href="#">Lisa Lee | Web Development</a>
+					<a class="navbar-brand" href="#">lisa lee | web design</a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
@@ -52,10 +52,10 @@
 				<div class="row">
 					<div class="col">
 						<div class="jumbotron">
-							<h1 class="display-4">Lisa Lee Web Development</h1>
-							<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+							<h1 class="display-4">lisa lee...web design</h1>
+							<p class="lead">welcome!</p>
 							<hr class="my-4">
-							<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+							<p>I'm Lisa, a web developer/designer, who is curious about uncovering the story behind you and your business.</p>
 							<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 						</div>
 					</div>
